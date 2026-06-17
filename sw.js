692294380673ebbdf822aee8ac666bf279ed041a
trippypaneer <joshi.shubham1818@gmail.com>
2026-06-18 00:15:21 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,7 +1,8 @@
 const CACHE_NAME = 'darshak-v2';
 const ASSETS = [
   'index.html',
-  'manifest.json'
+  'manifest.json',
+  'app-icon.png'
 ];
 
 self.addEventListener('install', (e) => {
@@ -13,5 +14,18 @@ self.addEventListener('install', (e) => {
 self.addEventListener('fetch', (e) => {
   e.respondWith(
     caches.match(e.request).then((response) => response || fetch(e.request))
+  );
+});
+
+// This forces the old cache to delete itself when a new version (v2) is found
+self.addEventListener('activate', (e) => {
+  e.waitUntil(
+    caches.keys().then((keyList) => {
+      return Promise.all(keyList.map((key) => {
+        if (key !== CACHE_NAME) {
+          return caches.delete(key);
+        }
+      }));
+    })
   );
 });
